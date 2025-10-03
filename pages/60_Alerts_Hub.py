a19dc4fd22d85ccd6b0559742fba36bb2ca0b92c
SmartMoneyCockpit <blainedares@gmail.com>
2025-10-03 16:38:59 -0700

    Delete pages/60_Alerts_Hub.py

--- pages/60_Alerts_Hub.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-from modules.alerts_hub import alerts_hub_view
-
-st.set_page_config(page_title="Alerts Hub — Amazon Cockpit", page_icon="🚨", layout="wide")
-alerts_hub_view()
